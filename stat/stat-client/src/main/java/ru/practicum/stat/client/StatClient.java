c04e1d1d88ed1977f72068ba914a7b9c48660e5f
Kekbaber <Kokmes45@gmail.com>
2026-09-07 21:18:12 +0300

    refactor: move StatClient bean configuration into stat-client module

--- stat/stat-client/src/main/java/ru/practicum/stat/client/StatClient.java
+++ stat/stat-client/src/main/java/ru/practicum/stat/client/StatClient.java
@@ -1,11 +1,9 @@
 package ru.practicum.stat.client;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.stat.dto.EndpointHit;
@@ -14,12 +12,11 @@ import ru.practicum.stat.dto.ViewStats;
 import java.util.List;
 
 @Slf4j
-@Component
 public class StatClient {
 
     private final RestClient restClient;
 
-    public StatClient(@Value("${stat.server.url:http://localhost:9090}") String baseUrl) {
+    public StatClient(String baseUrl) {
         this.restClient = RestClient.builder().baseUrl(baseUrl).build();
     }
 
--- stat/stat-client/src/main/java/ru/practicum/stat/client/StatClientAutoConfiguration.java
+++ stat/stat-client/src/main/java/ru/practicum/stat/client/StatClientAutoConfiguration.java
@@ -1,12 +1,11 @@
-package ru.practicum.main.config;
+package ru.practicum.stat.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import ru.practicum.stat.client.StatClient;
 
-@Configuration
-public class StatClientConfig {
+@AutoConfiguration
+public class StatClientAutoConfiguration {
 
     @Bean
     public StatClient statClient(@Value("${stat.server.url:http://localhost:9090}") String baseUrl) {
